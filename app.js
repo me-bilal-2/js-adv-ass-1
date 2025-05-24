b91b1d459ae1c8983acb3c3483e414beb9e694cc
me-bilal-2 <dexent.12345678@gmail.com>
2025-05-24 05:06:07 -0700

    this is my first assignment of advance module

---
+++ app.js
@@ -0,0 +1,56 @@
+// q1
+// const name = "John Doe";
+// const age = 30;
+// const occupation = "Software Developer";
+// const email = "john.doe@example.com";
+// const phoneNumber = "123-456-7890";
+// const instituteName = "Jawan Pakistan";
+// const education = "ABC";
+// const city = "Karachi";
+
+// let biodata = `Name: ${name} <br>Age: ${age} <br>Occupation: ${occupation} <br>Email: ${email} <br>Phone Number: ${phoneNumber} <br>Institute Name: ${instituteName} <br>Education: ${education} <br>City: ${city}`;
+
+// document.write(biodata);
+
+// // Q2:
+// const totalSubjects = 5;
+// const maxMarksPerSubject = 100;
+// const totalMaxMarks = totalSubjects * maxMarksPerSubject;
+// let mathMarks = 85;
+// let englishMarks = 92;
+// let scienceMarks = 78;
+// let historyMarks = 88;
+// let computerMarks = 95;
+// let totalObtainedMarks = mathMarks + englishMarks + scienceMarks + historyMarks + computerMarks;
+// let percentage = (totalObtainedMarks / totalMaxMarks) * 100;
+// let grade;
+// if (percentage >= 90) {
+//     grade = "A+";
+// } else if (percentage >= 80) {
+//     grade = "A";
+// } else if (percentage >= 70) {
+//     grade = "B";
+// } else if (percentage >= 60) {
+//     grade = "C";
+// } else if (percentage >= 50) {
+//     grade = "D";
+// } else {
+//     grade = "F";
+// }
+// const markSheetOutput = `.....STUDENT MARKSHEET....<br> Mathematics: ${mathMarks}/${maxMarksPerSubject}<br>English: ${englishMarks}/${maxMarksPerSubject}<br> Science: ${scienceMarks}/${maxMarksPerSubject}<br> History: ${historyMarks}/${maxMarksPerSubject} <br>Computer: ${computerMarks}/${maxMarksPerSubject} <br>Total Marks Obtained: ${totalObtainedMarks}/${totalMaxMarks} <br>Percentage: ${percentage.toFixed(2)}% <br>Grade: ${grade}`;
+// document.write(markSheetOutput);
+
+// Q3: 
+
+// var a = "Mohammad";
+// var abc;
+// var a = "faraz";
+// console.log(a);
+// var a = "Muhammad";
+// console.log(a);
+
+// function abc() {
+//   var a = "faraz";
+//   console.log(a); 
+// }
+// console.log(abc());
